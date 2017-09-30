87b50f28b2a2e36d7d6ac933e31aacc80fa024c6
soyetsin <soyetsin@gmail.com>
2017-09-30 20:04:09 +0800

    wancheng jinqi kaijiang

--- js/soye/recent_lottery.js
+++ js/soye/recent_lottery.js
@@ -1,4 +1,5 @@
 $(function () {
+    access_token = getCookie("access_token");  // 取token
     var pcount = 30
     var lotteryId = 1
 
@@ -11,7 +12,7 @@ $(function () {
         $(t).click(function () {
             $('.trend_tab_1 li').attr('class', '')
             $(this).attr('class', 'active')
-            pcount = $(this).attr('data-val')
+            data.pcount = $(this).attr('data-val')
             initopenNumsFun()
         })
     })
@@ -26,11 +27,29 @@ $(function () {
             },
             dataType: 'json',
             contentType: "application/json; charset=utf-8",  // json格式传给后端
-            url: action.forseti + 'api/openNums/bigDoubleCout',
-            data: JSON.stringify(data),  // json格式
+            url: action.forseti + 'api/openNums/bigDoubleCount',
+            data: data,  // json格式
             success: function (res) {
                 if (res.err == "SUCCESS") {
                     var data = res.data;
+                    console.log(data)
+                    var tr_html = ''
+                    $.each(data, function (i, e) {
+                        e.pcode = e.pcode % 1000
+                        if (e.pcode < 100) {
+                            e.pcode = '0' + e.pcode
+                        }
+                        e.winNumber = e.winNumber || ''
+                        e.winNumber = e.winNumber.split(',')
+                        tr_html += '<tr><td>' + e.pcode + '期</td><td><i>' + e.winNumber[0] + '</i><i>' + e.winNumber[1] + '</i><i>' + e.winNumber[2] + '</i><i>' + e.winNumber[3] + '</i><i>' + e.winNumber[4] + '</i></td>'
+                        $.each(e.countMap, function (i, t) {
+                            if (i != 2) {
+                                tr_html += '<td>' + t + '</td>'
+                            }
+                        })
+                        tr_html += '</tr>'
+                    })
+                    $(".so-bigDoubleCount").html(tr_html)
                 }
             },
             error: function (err) {
