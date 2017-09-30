53516b48fc4dffedac79a322642c74aa5394c93f
soyetsin <soyetsin@gmail.com>
2017-09-30 11:16:53 +0800

    新增投注详情-未中奖

--- js/soye/betting_record.js
+++ js/soye/betting_record.js
@@ -36,6 +36,8 @@ $(function () {
     * orderStatus,注单状态，1等待开奖,31未中奖,32已派彩,4用户撤单,5系统撤单,6中奖停追,71存在异常
     * */
     function getBetRecord(page, type, status) {
+        $('.tab_content_' + status).html("");
+        $('.tab_track_' + status).html("");
         var seadata = {
             "page": page,   //页数，从1开始
             "pageSize": 200,  //每页行数
@@ -68,33 +70,47 @@ $(function () {
                     daysArr[v.pcode].push(v);
 
                 });
-
                 console.log(daysArr);
-
                 var restr = '';
+                var li_html = ''
                 //   $('.bet-recode-all').append(str) ;
                 if (type == 1) {
                     $.each(daysArr, function (j, m) {
                         restr += '<li class="slide_toggle bet_day ">' +
                             '<div class="panel_title"> <strong class="title-data">' + j.toString().substring(4, 6) + '月' + j.toString().substring(6, 8) + '日 </strong><span></span> </div>' +
                             '<ul class="panel bet-recode-all">';
-
                         $.each(m, function (i, v) {
-                            restr += '<li class="bet_data" data-status="not_open">' +
-                                '<a href="betting_record_dt01.html">' +
-                                '<div class="badge ssc_badge"></div>' +
-                                '<div class="lottery_t ssc">' +
-                                '<p>' + v.lotteryName + ' - <span>' + v.playName + '</span></p> <strong>' + roundAmt(v.betAmount) + '</strong> </div>' +
-                                '<div class="status status0"' + v.orderStatus + '>' +
-                                '<span>' + v.orderStatusName + '</span>' +
-                                '<div></div>' +
-                                '</div>' +
-                                ' </a>' +
-                                '</li>';
+                            var statusStr = '';
+                            if (status == 1) {
+                                statusStr = '全部'
+                            } else if (status == 2) {
+                                statusStr = '等待开奖'
+                            } else if (status == 3) {
+                                statusStr = '已派彩'
+                            } else if (status == 4) {
+                                statusStr = '未中奖'
+                            }
+                            if (statusStr == v.orderStatusName || statusStr == '全部') {
+                                var jsonStr = encodeURI("betting_record_dt01.html?data=" + JSON.stringify(v));
+                                li_html += '<li class="bet_data" data-status="not_open">' +
+                                    '<a href="' + jsonStr + '">' +
+                                    '<div class="badge ssc_badge"></div>' +
+                                    '<div class="lottery_t ssc">' +
+                                    '<p>' + v.lotteryName + ' - <span>' + v.playName + '</span></p> <strong>' + roundAmt(v.betAmount) + '</strong> </div>' +
+                                    '<div class="status status0"' + v.orderStatus + '>' +
+                                    '<span>' + v.orderStatusName + '</span>' +
+                                    '<div></div>' +
+                                    '</div>' +
+                                    ' </a>' +
+                                    '</li>';
+                            }
 
                         });
+                        restr += li_html;
                         restr += '</ul></li>';
-
+                        if (li_html == "") {
+                            restr = ""
+                        }
                     });
                     $('.tab_content_' + status).html(restr);
                 } else if (type == 2) {
@@ -102,23 +118,36 @@ $(function () {
                         restr += '<li class="slide_toggle bet_day ">' +
                             '<div class="panel_title"> <strong class="title-data">' + j.toString().substring(4, 6) + '月' + j.toString().substring(6, 8) + '日 </strong><span></span> </div>' +
                             '<ul class="panel bet-recode-all">';
-
                         $.each(m, function (i, v) {
-                            restr += '<li class="bet_data" data-status="not_open">' +
-                                '<a href="betting_record_dt01.html">' +
-                                '<div class="badge ssc_badge"></div>' +
-                                '<div class="lottery_t ssc">' +
-                                '<p>' + v.lotteryName + ' - <span>' + v.playName + '</span></p> <strong>' + roundAmt(v.betAmount) + '</strong> </div>' +
-                                '<div class="status status0"' + v.orderStatus + '>' +
-                                '<span>' + v.chaseStatusName + '</span>' +
-                                '<div></div>' +
-                                '</div>' +
-                                ' </a>' +
-                                '</li>';
-
+                            var statusStr = '';
+                            if (status == 1) {
+                                statusStr = '全部'
+                            } else if (status == 2) {
+                                statusStr = '进行中'
+                            } else if (status == 3) {
+                                statusStr = '已结束'
+                            } else if (status == 4) {
+                                statusStr = '已终止'
+                            }
+                            if (statusStr == v.chaseStatusName || statusStr == '全部') {
+                                li_html += '<li class="bet_data" data-status="not_open">' +
+                                    '<a href="trace_record_dt01.html">' +
+                                    '<div class="badge ssc_badge"></div>' +
+                                    '<div class="lottery_t ssc">' +
+                                    '<p>' + v.lotteryName + ' - <span>' + v.playName + '</span></p> <strong>' + roundAmt(v.betAmount) + '</strong> </div>' +
+                                    '<div class="status status0"' + v.orderStatus + '>' +
+                                    '<span>' + v.chaseStatusName + '</span>' +
+                                    '<div></div>' +
+                                    '</div>' +
+                                    ' </a>' +
+                                    '</li>';
+                            }
                         });
+                        restr += li_html;
                         restr += '</ul></li>';
-
+                        if (li_html == "") {
+                            restr = ""
+                        }
                     });
                     $('.tab_track_' + status).html(restr);
                 }
@@ -143,7 +172,6 @@ $(function () {
             getBetRecord(1, 2, val)
 
         });
-
     }
 
     recodeChange();
