595960ab24525bb845957eaad415238cac70d0e3
Sam <qianduan123@outlook>
2017-11-03 16:11:25 +0800

    codify api path

--- src/components/publicTemplate/AutoCloseDialog.vue
+++ src/components/publicTemplate/AutoCloseDialog.vue
@@ -45,4 +45,13 @@ export default {
                     position: fixed;
                     z-index: 1000;
                   }
+    .so-tip-pop > div{
+      border:none;
+      width: 8.5rem;
+      height: 5rem;
+    }
+    .so-tip-pop img{
+        width: 5rem;
+        margin-top: 1rem;
+    }
 </style>
