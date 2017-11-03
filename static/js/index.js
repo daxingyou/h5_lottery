936abb7fdbe5cb475b123851cd95bc965390e1a6
janice <janice.chiang@kingbaytech.com>
2017-11-03 19:46:29 +0800

    禁止遮罩层以下屏幕滑动

--- static/js/index.js
+++ static/js/index.js
@@ -253,6 +253,15 @@ function initPopWafa() {
     });
 }
 
+//禁止遮罩层以下屏幕滑动
+$(document).on("touchmove", function (e) {
+    var e = e || event,
+        target = e.target || e.srcElement;
+    if (e.target.className.indexOf("so-shade") >= 0) { //className為弹窗的蒙层的类名
+        e.preventDefault();
+    }
+})
+
 // demo dialog
 // 本彩种暂停销售
 // function initPop01() {
