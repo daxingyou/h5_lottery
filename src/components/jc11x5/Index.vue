f0a69ff4fab73ce99a69f99a45767153ae64f83e
webb <bc001122@outlook.com>
2017-11-06 11:16:20 +0800

    bug

--- src/components/jc11x5/Index.vue
+++ src/components/jc11x5/Index.vue
@@ -51,8 +51,9 @@
                                 <ul>
                                     <li>{{lastTermStatic.total}}</li>
                                     <li>{{lastTermStatic.sizer}}</li>
-                                    <li>{{lastTermStatic.longer}}</li>
                                     <li>{{lastTermStatic.doubler}}</li>
+                                    <li>{{lastTermStatic.longer}}</li>
+
                                 </ul>
                             </div>
                         </div>
