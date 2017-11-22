ec5ee6ebb5566d0a9d3d71416608f0b5ed195eb6
webb <bc001122@outlook.com>
2017-11-22 13:55:33 +0800

    bug

--- src/components/pk10/Index.vue
+++ src/components/pk10/Index.vue
@@ -437,7 +437,7 @@ export default {
         timerBegin:function(){
             var that = this ;
             that.lotteryDataFetch().then(()=>{
-                console.log(that.nowover_time+'dsf')
+
                 that.$refs.countdownTimer && that.$refs.countdownTimer.timerInit(that.sys_time, that.now_time, that.nowover_time);
             })
             that.entertainStatus = false;
