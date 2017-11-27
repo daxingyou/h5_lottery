73010671b9ad421a789c8a94db306ee6f9a2215e
Sam <qianduan123@outlook>
2017-11-27 14:22:40 +0800

    hh

--- src/components/lobbyTemplate/withdrawals_bind.vue
+++ src/components/lobbyTemplate/withdrawals_bind.vue
@@ -20,7 +20,7 @@
                     </div>
                     <label class=" error-message"></label>
                 </fieldset>
-                <fieldset>/9
+                <fieldset>
                     <div class="form_g text">
                         <legend>选择银行</legend>
                         <select name="" v-model="bankId" class="bankselect">
